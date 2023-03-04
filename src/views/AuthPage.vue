2c901efae8c5c0ee19f9ba1e4347bce279986e3f
Daniil <dvinakovich@gmail.com>
2023-03-04 22:55:11 +0700

    Added styles for auth

--- src/views/AuthPage.vue
+++ src/views/AuthPage.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>login</div>
+  <div class="auth">
+    <div class="container auth__container">
+      <h1 class="auth__title">Sign in</h1>
+      <form class="auth__form">
+        <input type="text" class="auth__input input-reset" placeholder="Login" required>
+        <input type="password" class="auth__input input-reset" placeholder="Password" required>
+        <button class="auth__btn btn-reset" type="submit">Sing In</button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
