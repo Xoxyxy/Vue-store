e614b58129a2cd1fad0186237cecffb954ecf8b3
Daniil <dvinakovich@gmail.com>
2023-03-04 22:02:42 +0700

    Added styles + fonts

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
+
+import '@/assets/scss/vendor.scss'
+import '@/assets/scss/main.scss'
 
 const app = createApp(App)
 
